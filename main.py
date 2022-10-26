15a0f965d3abe0bc906c4152763246c1b6acd447
chainofexecution <ianmoyer7@gmail.com>
2022-10-26 12:34:44 -0400

    Separated HTML content from main.py Added a forever loop after socket bind to supress errors of address in use when a connection closes

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import network
 import socket
 import time
+import io
 
-html = """<!DOCTYPE html>
-<html>
-    <head> <title>ESP8266 Test</title> </head>
-    <body> <h1>ESP8266 Test Site!</h1>
-    </body>
-</html>
-"""
-resp='HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'
+with io.open('www/config.html', 'r', encoding="utf-8") as f:
+    config_html = f.read()
+# """<!DOCTYPE html>
+# <html>
+#     <head> <title>ESP8266 Test</title> </head>
+#     <body> <h1>ESP8266 Test Site!</h1>
+#     </body>
+# </html>
+# """
+
+http_response_header='HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'
 ssid = 'AdBinder'
 psk = 'default1'
 
 WPA2_PSK = 3 # Authmode for WLAN IF
-
+print(config_html)
+network.phy_mode(network.MODE_11N)
 wlan = network.WLAN(network.AP_IF)
 wlan.active(True)
-network.phy_mode(network.MODE_11N)
 wlan.ifconfig(('192.168.0.1', '255.255.255.0', '192.168.0.1', '192.168.0.1'))
 wlan.config(essid=ssid, password=psk, authmode=WPA2_PSK, channel=11)
 print(f'Broadcasting as AP IF on SSID \'{ssid}\' with PSK \'{psk}\'')
@@ -31,9 +35,10 @@ while True:
     s = socket.socket()
     s.bind(addr)
     print('Listening for HTTP traffic on port 80')
-    s.listen(1)
-    conn,addr = s.accept()
-    print(f'Sending response to STA over port 80')
-    conn.send(resp)
-    conn.send(html)
-    conn.close()
+    while True:
+        s.listen(1)
+        conn,addr = s.accept()
+        print(f'Sending response to STA over port 80')
+        conn.send(http_response_header)
+        conn.send(config_html)
+        conn.close()
